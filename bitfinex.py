720c2974a252c165920f52c3ab0a31be3eee6bf8
Matthew Madurski <matt@matthewmadurski.com>
2014-11-01 16:01:46 -0400

    Added basic bitfinex support

--- bitfinex.py
+++ bitfinex.py
@@ -1,11 +1,12 @@
 from decimal import Decimal
-from helpers import get_datetime, get_response
+from helpers import *
+import requests
 
-TICKER_URL = 'https://bitstamp.net/api/ticker/'
+TICKER_URL = 'https://api.bitfinex.com/v1/pubticker/btcusd'
 
 def get_current_price():
     data = get_response(TICKER_URL)
-    price = data['last']
+    price = data['last_price']
     return Decimal(price)
 
 def get_current_bid():
