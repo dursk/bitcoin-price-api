4eb62179bb341a8ccf0b57723bf42d40f4077972
Matthew Madurski <matt@matthewmadurski.com>
2014-11-16 00:30:18 -0500

    remove unneeded import

--- exchanges/huobi.py
+++ exchanges/huobi.py
@@ -1,20 +1,19 @@
 from decimal import Decimal
 from helpers import *
-import requests
 
 TICKER_URL = 'https://market.huobi.com/staticmarket/ticker_btc_json.js'
 
 def get_current_price():
-	data = get_response(TICKER_URL)
-	price = data['ticker']['last']
-	return Decimal(price)
-	
+        data = get_response(TICKER_URL)
+        price = data['ticker']['last']
+        return Decimal(price)
+
 def get_current_bid():
-	data = get_response(TICKER_URL)
-	price = data['ticker']['buy']
-	return Decimal(price)
-	
+        data = get_response(TICKER_URL)
+        price = data['ticker']['buy']
+        return Decimal(price)
+
 def get_current_ask():
-	data = get_response(TICKER_URL)
-	price = data['ticker']['sell']
-	return Decimal(price)
+        data = get_response(TICKER_URL)
+        price = data['ticker']['sell']
+        return Decimal(price)
