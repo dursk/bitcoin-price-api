e37efe3bbf59c271f1d131758569a5ab47a8b4c6
Joseph C Wang <joequant@gmail.com>
2016-03-21 03:35:57 +0800

    add pypi

---
+++ setup.py
@@ -0,0 +1,22 @@
+# Copyright (C) 2015 Bitquant Research Laboratories (Asia) Limited
+# Released under the Simplified BSD License
+
+from setuptools import (
+    setup,
+    find_packages,
+    )
+
+setup(
+    name="bitcoin-price-api",
+    version = "0.0.1",
+    author="Matthew Madurski",
+    author_email='madurskimr@gmail.com',
+    url="https://github.com/dursk/bitcoin-price-api",
+    description="API's for bitcoin exchanges",
+    long_description="""Price API's for bitcoin exchanges""",
+    license="MIT",
+    packages=['exchanges'],
+    install_requires = ["requests"],
+    use_2to3 = True
+)
+                                
