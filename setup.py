b927dcf630cd62ae295c6ecd9a76d710917fd19c
dursk <matt@matthewmadurski.com>
2016-03-23 08:43:50 -0400

    setuppy cleanup

--- setup.py
+++ setup.py
@@ -4,20 +4,18 @@
 from setuptools import (
     setup,
     find_packages,
-    )
+)
 
 setup(
-    name="bitcoin-price-api",
-    version = "0.0.2",
-    author="Matthew Madurski",
+    name='bitcoin-price-api',
+    version = '0.0.3',
+    author='Matthew Madurski',
     author_email='madurskimr@gmail.com',
-    url="https://github.com/dursk/bitcoin-price-api",
+    url='https://github.com/dursk/bitcoin-price-api',
     description="API's for bitcoin exchanges",
-    long_description="""Price API's for bitcoin exchanges""",
-    license="MIT",
+    long_description='''Price API's for bitcoin exchanges''',
+    license='MIT',
     packages=['exchanges'],
-    install_requires = ["requests", "python-dateutil"],
-    scripts = ['bitcoin-price.py'],
+    install_requires = ['requests', 'python-dateutil'],
     use_2to3 = True
 )
-                                
